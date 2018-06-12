f55635f14d45867fa9373abee08d82d5e5dfaf65
Alex Yu <alexyu0@ip-172-30-2-135.ec2.internal>
2018-06-12 14:55:35 +0000

    rm dbConfig.py

--- dbConfig.py
+++
@@ -1,5 +0,0 @@
-#Do not commit login info to github !!
-#There is security risk to leak the ip address of our mongodb server
-HOSTNAME = "xxx.xxx.xxx.xxx"
-USERNAME = "xxxxx"
-PASSWORD = "xxxxxxxxx"
